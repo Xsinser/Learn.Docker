b03c75efef1411cb9bdb4af5dbc494fd015b11f8
Xsinser <xsinser175@gmail.com>
2024-12-23 00:42:52 +0300

    Добавьте файлы проекта.

---
+++ Learn.Docker.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Learn.Docker.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
